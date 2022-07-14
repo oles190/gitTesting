4854fe018fd1aec52c071f4676f9051d1c9d04a5
Oles Doskuch <odoskuch@vector-software.com>
2022-07-14 16:49:46 +0300

    marge

--- src/main/java/com/example/demo/Goil.java
+++ src/main/java/com/example/demo/Goil.java
@@ -2,4 +2,10 @@ package com.example.demo;
 
 public class Goil {
     ///ddasdas/dasd/ad/asd/asd/ad/asad/
+    public static void main(String[] args) {
+
+        System.out.println(
+                "ssdad"
+        );
+    }
 }
